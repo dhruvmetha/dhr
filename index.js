d43562aec04a7f583ff84af671bc530cab973dac
Saif <saifur.cs17@bmsce.ac.in>
2018-04-02 18:08:50 +0530

    initial

---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var events = require('./events');
+var team = require('./team')
+var app = express();
+
+process.env.NODE_ENV = 'production';
+
+app.set("views", path.resolve(__dirname, "views"));
+var publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+app.get('/', function(req, res){
+	 res.render("index"); 
+});
+
+app.get('/events', function(req, res){
+		app.locals.json = events;
+
+	  res.render("categories"); 
+});
+
+
+
+app.get('/schedule', function(req, res){
+		
+
+	  res.render("schedule"); 
+});
+
+
+
+app.get('/events/*', function(req, res){
+	 app.locals.events  = events[req.originalUrl.slice(8, req.originalUrl.length)].events;
+	 app.locals.category = events[req.originalUrl.slice(8, req.originalUrl.length)].name;
+
+	  res.render("events"); 
+});
+
+app.get('/about-us', function(req, res){
+	 app.locals.faculty  = team.faculty;
+	 app.locals.core = team.core;
+	  res.render("about-us"); 
+});
+
+
+app.get('/*',function(req, res){
+	res.render("404");
+});
+
+app.set("view engine", "ejs");
+
+app.listen(3000, function(){
+	console.log("Started at 3000");
+})
---
+++ team.js
@@ -0,0 +1,72 @@
+module.exports = {
+	"faculty":[
+		{
+			"name":"Dr BV Ravishankar",
+			"number":"",
+			"desc":"Vice Principal",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-01%20at%201.11.23%20PM.jpeg?alt=media&token=6140aa87-6032-4a4f-8c79-f69b7bedfeeb"
+		},
+		{
+			"name":"Dr Ravishankar Deekshit",
+			"number":"",
+			"desc":"Working Chairman Utsav 2018",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-01%20at%201.12.08%20PM.jpeg?alt=media&token=b437573a-2332-4754-a664-158239607358"
+		},
+		{
+			"name":"Dr M Shivarama Reddy ",
+			"number":"",
+			"desc":"Organising Secretary Utsav 2018",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-01%20at%201.13.18%20PM.jpeg?alt=media&token=fc699763-03b9-4be1-b128-22639310cdb9"
+		},
+		{
+			"name":"Dr. Umadevi V",
+			"img":"https://sites.google.com/site/drvumadevi/_/rsrc/1320312360375/home/Uma-Photo1.JPG?height=200&width=200",
+			"desc" :"Web Presence and Member Database",
+			
+		}
+		
+	],
+
+	"core":[{
+			"name":"Sagar Jogdanker",
+			"number":"9482634433",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.15.24%20PM.jpeg?alt=media&token=a8d09374-e2d7-4dd2-b2f3-81a362fd449e"
+			},
+			{
+			"name":"Faraz Khan",
+			"number":"8050332624",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.15.48%20PM.jpeg?alt=media&token=38746a60-1164-44a5-ba84-86b2570b4ce0"
+			},
+			{
+			"name":"Harshith Muppiri",
+			"number":"8105554114",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.16.02%20PM.jpeg?alt=media&token=a2d7980a-8e66-4c71-a025-556f1c4a02a6"
+			},
+			{
+			"name":"Sandeep Kumar M",
+			"number":"7795039253",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.38.28%20PM.jpeg?alt=media&token=c5193aa6-7bd1-4975-9f56-7cf510459619"
+			},
+			{
+			"name":"Srujan MJ",
+			"number":"8867559212",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.41.24%20PM.jpeg?alt=media&token=e88f2e14-8e39-4459-9fb0-740f53229695"
+			},
+			{
+			"name":"Niyathi Singh",
+			"number":"9008005000",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.41.36%20PM.jpeg?alt=media&token=9b26bbe8-b6d4-4ec8-8da4-555a0cbac382"
+			},
+			{
+			"name":"Praveen Kumar B",
+			"number":"8095840515",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.41.53%20PM.jpeg?alt=media&token=9dad9073-de41-47e5-8a7b-439e66dae7b0"
+			},
+			{
+			"name":"Gajanan Kamble",
+			"number":"8147688713",
+			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-01%20at%205.59.00%20PM.jpeg?alt=media&token=677f27fb-163e-4d14-8b7a-0ce910d0f326"
+		}
+	]
+
+};
