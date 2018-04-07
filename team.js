f269b128043407fafd5075d8ad81e7bb639eb318
Saif <saifur.cs17@bmsce.ac.in>
2018-04-07 10:06:21 +0530

    team new image links

--- team.js
+++ team.js
@@ -4,31 +4,31 @@ module.exports = {
 			"name":"Dr BV Ravishankar",
 			"number":"",
 			"desc":"Vice Principal",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-01%20at%201.11.23%20PM.jpeg?alt=media&token=6140aa87-6032-4a4f-8c79-f69b7bedfeeb"
+			"img" : "https://imgur.com/3vsPcYj.jpg"
 		},
 		{
 			"name":"Dr Ravishankar Deekshit",
 			"number":"",
 			"desc":"Working Chairman Utsav 2018",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-01%20at%201.12.08%20PM.jpeg?alt=media&token=b437573a-2332-4754-a664-158239607358"
+			"img" : "https://imgur.com/NvCFBmY.jpg"
 		},
 		{
 			"name":"Dr M Shivarama Reddy",
 			"number":"",
 			"desc":"Organising Secretary Utsav 2018",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-01%20at%201.13.18%20PM.jpeg?alt=media&token=fc699763-03b9-4be1-b128-22639310cdb9"
+			"img" : "https://imgur.com/kEiztok.jpg"
 		},
 		{
 			"name":"Dr Ranga Swamy",
 			"number":"",
 			"desc":"Treasurer",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-03%20at%204.01.52%20PM.jpeg?alt=media&token=8fce0e06-2596-4601-833c-53a2318f775c"
+			"img" : "https://imgur.com/Hb1yiqE.jpg"
 		},
 		{
 			"name":"Prathima Bhat K",
 			"number":"",
 			"desc":"Treasurer",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-03%20at%204.01.51%20PM.jpeg?alt=media&token=bae2e83d-7cce-48e8-a7a2-7a3af6791bf4"
+			"img" : "https://imgur.com/hRNlSBh.jpg"
 		}
 
 		
@@ -38,42 +38,42 @@ module.exports = {
 	"core":[{
 			"name":"Sagar Jogdanker",
 			"number":"9482634433",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.15.24%20PM.jpeg?alt=media&token=a8d09374-e2d7-4dd2-b2f3-81a362fd449e"
+			"img" : "https://imgur.com/iwd4vur.jpg"
 			},
 			{
 			"name":"Faraz Khan",
 			"number":"8050332624",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.15.48%20PM.jpeg?alt=media&token=38746a60-1164-44a5-ba84-86b2570b4ce0"
+			"img" : "https://imgur.com/HZNdQB6.jpg"
 			},
 			{
 			"name":"Gajanan Kamble",
 			"number":"8147688713",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FFaculty%2FWhatsApp%20Image%202018-04-01%20at%205.59.00%20PM.jpeg?alt=media&token=677f27fb-163e-4d14-8b7a-0ce910d0f326"
+			"img" : "https://imgur.com/uZlTqRy.jpg"
 		    },
 			{
 			"name":"Harshith Muppiri",
 			"number":"8105554114",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.16.02%20PM.jpeg?alt=media&token=a2d7980a-8e66-4c71-a025-556f1c4a02a6"
+			"img" : "https://imgur.com/FNm9C1n.jpg"
 			},
 			{
 			"name":"Sandeep Kumar M",
 			"number":"7795039253",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FIMG-20180403-WA0041.jpg?alt=media&token=56860fd1-4f38-4456-8e2a-726a42422fe3"
+			"img" : "https://imgur.com/959mS4J.jpg"
 			},
 			{
 			"name":"Srujan MJ",
 			"number":"8867559212",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.41.24%20PM.jpeg?alt=media&token=e88f2e14-8e39-4459-9fb0-740f53229695"
+			"img" : "https://imgur.com/gsKP1lr.jpg"
 			},
 			{
 			"name":"Niyathi Singh",
 			"number":"9008005000",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.41.36%20PM.jpeg?alt=media&token=9b26bbe8-b6d4-4ec8-8da4-555a0cbac382"
+			"img" : "https://imgur.com/BsGO9b0.jpg"
 			},
 			{
 			"name":"Praveen Kumar B",
 			"number":"8095840515",
-			"img" : "https://firebasestorage.googleapis.com/v0/b/utsavimages-32c64.appspot.com/o/Team%2FCore%2FWhatsApp%20Image%202018-03-31%20at%207.41.53%20PM.jpeg?alt=media&token=9dad9073-de41-47e5-8a7b-439e66dae7b0"
+			"img" : "https://imgur.com/pSCPnD7.jpg"
 			}
 			
 	]
